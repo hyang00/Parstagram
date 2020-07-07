c3be0faa3dc53bf2d0c6830fef0271fb4bb253aa
Hannah Yang <hannahyang@fb.com>
2020-07-06 17:42:28 -0700

    Working login, bottom navigation view, composeFragment, postFragment

---
+++ app/src/main/java/com/example/parstagram/fragments/ProfileFragment.java
@@ -0,0 +1,37 @@
+package com.example.parstagram.fragments;
+
+import android.util.Log;
+
+import com.example.parstagram.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostFragment {
+    @Override
+    protected void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // also want the user info associated w/ the post
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CEATED_KEY);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue w/ getting posts", e);
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
